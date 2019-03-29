98913f12a3ee3d31f10b16c248642bac223376a0
James Wong <jwong@redhat.com>
2019-03-29 11:19:05 -0400

    for outlier detection: collect data from host inventory svc

--- target_worker/__init__.py
+++ target_worker/__init__.py
@@ -104,6 +104,18 @@ if os.environ.get('INPUT_DATA_FORMAT') == 'TOPOLOGY':
     }
 
 
+elif os.environ.get('INPUT_DATA_FORMAT') == 'HOST':
+    logger.info('Target Worker is Host')
+    host_inventory_url = os.environ.get('HOST_INVENTORY_URL')
+
+    if not host_inventory_url:
+        logger.error('Environment not set properly, '
+                     'missing HOST_INVENTORY_URL')
+        sys.exit(1)
+    NAME = 'worker_host'
+    INFO = {'host_inventory_url': host_inventory_url}
+
+
 else:
     logger.info('Target Worker is Clustering')
 
