0d38b633c64b5cc6ea31e1f79d61bc331ae70758
James Wong <jwong@redhat.com>
2019-04-03 13:19:05 -0400

    Added env GUNICORN_WORKER to open up for other worker_class

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ bind = '127.0.0.1:8000'
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 timeout = 120
+worker_class = os.environ.get('GUNICORN_WORKER', 'sync')
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
