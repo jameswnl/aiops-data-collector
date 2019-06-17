4b3aaf4fc9bfa192521690fdf35acc42887610fe
James Wong <2421248+jameswnl@users.noreply.github.com>
2019-06-17 16:07:55 -0400

    start.py

--- start.py
+++ start.py
@@ -1,7 +1,30 @@
+import argparse
+
 from collector import WORKER
 
-def run():
-    acc = {'b64_identity': b'eyJpZGVudGl0eSI6eyJhY2NvdW50X251bWJlciI6IjE0NjAyOTAifX0=', 'account_id': 1460290}
-    WORKER(None, 'jobid=122', None, acc)
 
-run()
+def run(args):
+    acc = {"x-rh-identity": args.b64_identity, 'account_id': args.account_id}
+    WORKER(None, args.job_id or 'N/A', args.output_path, acc)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  # arguments
+  parser.add_argument(
+      '--account_id', type=str, required=True, help='Account ID'
+  )
+  parser.add_argument(
+      '--b64_identity', type=str, required=True, help='b64_identity'
+  )
+  parser.add_argument(
+      '--output_path', type=str, required=True, help='output path'
+  )
+  parser.add_argument(
+      '--job_id', type=str, required=True, help='Job ID'
+  )
+
+  # Parse all arguments
+  args = parser.parse_args()
+  
+run(args)
